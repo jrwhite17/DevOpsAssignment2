3770708c7496caf379b5594bf5ae53c8b6041c86
jrwhite17 <johnrileywhite@gmail.com>
2017-08-26 10:24:51 -0500

    polishing up automated test script

--- automatedTests/cloudFormationTest/CloudFormationTest.py
+++ automatedTests/cloudFormationTest/CloudFormationTest.py
@@ -12,44 +12,58 @@ import re
 import time
 import os
 import subprocess
-import request
+import requests
 from pprint import pprint
 
-#Stack Name
-STACK_NAME="jwhiteStack"
-#Stack Parameters
-JENKINS_EC2_INSTANCE="t2.small"
-JENKINS_PASSSWORD="qwer"
-JENKINS_USERNAME="jwhite"
-SSH_PEM_KEY_INPUT="jrw_key_pair"
+#Function
+def testJenkinsCFTemplate(stack_name,jenkins_ec2_instance,jenkins_password,jenkins_username,ssh_pem_key_input):
+	#Stack Name
+	STACK_NAME=stack_name
+	#Stack Parameters
+	JENKINS_EC2_INSTANCE=jenkins_ec2_instance
+	JENKINS_PASSSWORD=jenkins_password
+	JENKINS_USERNAME=jenkins_username
+	SSH_PEM_KEY_INPUT=ssh_pem_key_input
 
-#Request stack
-CREATE_STACK_CMD = ("aws cloudformation create-stack \\"
-"--stack-name "+STACK_NAME+" \\"
-"--template-body file://../../cloudformation/cloudformation_devops_assignment2.template \\"
-"--parameters ParameterKey=JenkinsEC2Instance,ParameterValue="+JENKINS_EC2_INSTANCE+" \\"
-"ParameterKey=JenkinsPassword,ParameterValue="+JENKINS_PASSSWORD+" \\"
-"ParameterKey=JenkinsUsername,ParameterValue="+JENKINS_USERNAME+" \\"
-"ParameterKey=sshPemKeyInput,ParameterValue="+SSH_PEM_KEY_INPUT+"")
-os.system(CREATE_STACK_CMD)
+	#Request stack
+	CREATE_STACK_CMD = ("aws cloudformation create-stack \\"
+	"--stack-name "+STACK_NAME+" \\"
+	"--template-body file://../../cloudformation/cloudformation_devops_assignment2.template \\"
+	"--parameters ParameterKey=JenkinsEC2Instance,ParameterValue="+JENKINS_EC2_INSTANCE+" \\"
+	"ParameterKey=JenkinsPassword,ParameterValue="+JENKINS_PASSSWORD+" \\"
+	"ParameterKey=JenkinsUsername,ParameterValue="+JENKINS_USERNAME+" \\"
+	"ParameterKey=sshPemKeyInput,ParameterValue="+SSH_PEM_KEY_INPUT+"")
+	os.system(CREATE_STACK_CMD)
 
-#Give Cloudformation time to deploy stack
-time.sleep(120)
+	#Give Cloudformation time to deploy stack
+	time.sleep(120)
 
-#Write Stack Output to /tmp/stackOutput.json
-DESCRIBE_STACK_CMD="aws cloudformation describe-stacks --stack-name "+STACK_NAME
-stack_output = subprocess.check_output(DESCRIBE_STACK_CMD, shell=True)
+	#Write Stack Output to /tmp/stackOutput.json
+	DESCRIBE_STACK_CMD="aws cloudformation describe-stacks --stack-name "+STACK_NAME
+	stack_output = subprocess.check_output(DESCRIBE_STACK_CMD, shell=True)
 
-#Load Json and Extract Jenkins URL
-stack_output_json = json.loads(stack_output)
-Jenkins_URL=str(stack_output_json['Stacks'][0]['Outputs'][0]['OutputValue'])
+	#Load Json and Extract Jenkins URL
+	stack_output_json = json.loads(stack_output)
+	Jenkins_URL=str(stack_output_json['Stacks'][0]['Outputs'][0]['OutputValue'])
 
-pprint("Jenkins URL: " + Jenkins_URL)
+	pprint("Jenkins URL: " + Jenkins_URL)
 
-#Set timeout for 15 minutes
-t_end = time.time() + 60 * 15
-while time.time() < t_end:
-	r = requests.get(Jenkins_URL)
-    if r.status_code == 200:
+	#Set timeout for 15 minutes
+	t_end = time.time() + 60 * 15
+	while time.time() < t_end:
+	try:
+		r = requests.get(Jenkins_URL)
+		if r.status_code == 200:
 		pprint("Jenkins is running!")
+		pprint(t_end-time.time())
 		break
+	except requests.ConnectionError, e:
+		pass
+		#Do nothing
+		
+		
+		
+#MAIN
+testJenkinsCFTemplate("test1","t2.small","root","root","jrw_key_pair")
+testJenkinsCFTemplate("test2","t2.small","root","root","jrw_key_pair")
+testJenkinsCFTemplate("test3","t2.small","root","root","jrw_key_pair")
