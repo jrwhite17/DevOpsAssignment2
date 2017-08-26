e85b875a6140ee983e255d8bbbad899cd7dfa6ed
jrwhite17 <johnrileywhite@gmail.com>
2017-08-25 21:27:34 -0500

    test stack name replacement

--- jenkins_config/create_job.groovy
+++ jenkins_config/create_job.groovy
@@ -7,7 +7,10 @@ CloudformationData = new XmlSlurper().parse("/tmp/stackData.xml")
 def stackName = CloudformationData.stackName.text()
 
 //Add Stack Name to job config
-ant.replace(file: "/tmp/Deploy_ELK_Stack_Jenkins_Job_config.xml", token: "DevOpsAssignment2STACK", value: stackName)
+def file = new File('/tmp/stackData.xml')
+def newConfig = file.text.replace('DevOpsAssignment2STACK', stackName)
+file.text = newConfig
+
 
 //Deploy_ELK_Stack_Jenkins_Job_config.xml is located in /tmp
 //Moved to this location in the userdata portion of the CloudFormation template
@@ -17,3 +20,9 @@ def xmlStream = new ByteArrayInputStream( configXML.getBytes() )
 
 //Create Job
 Jenkins.instance.createProjectFromXML(jobName, xmlStream)
+
+
+
+def copyAndReplaceText(source, dest, Closure replaceText){
+    dest.write(replaceText(source.text))
+}
