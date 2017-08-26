e3146c7e413786ae8640d275e467dbc557629ede
jrwhite17 <johnrileywhite@gmail.com>
2017-08-26 07:35:39 -0500

    Testing Jenkins URL extraction using Bash

--- automatedTests/cloudFormationTest/CloudFormationTest.sh
+++ automatedTests/cloudFormationTest/CloudFormationTest.sh
@@ -18,6 +18,7 @@ JENKINS_USERNAME="jwhite"
 SSH_PEM_KEY_INPUT="jrw_key_pair"
 
 #Request stack
+echo "Requesting stack named "STACK_NAME
 aws cloudformation create-stack \
 --stack-name $STACK_NAME \
 --template-body file://../../cloudformation/cloudformation_devops_assignment2.template \
@@ -27,6 +28,9 @@ ParameterKey=JenkinsUsername,ParameterValue=$JENKINS_USERNAME \
 ParameterKey=sshPemKeyInput,ParameterValue=$SSH_PEM_KEY_INPUT
 
 #Give Cloudformation time to deploy stack
+echo "Waiting 2 minutes for stack to be deployed."
 sleep 120
 
-aws cloudformation describe-stacks --stack-name $STACK_NAME
+echo "Getting Jenkins URL"
+aws cloudformation describe-stacks --stack-name $STACK_NAME >> /tmp/stackOutput.json
+grep -Po '"text":.*?[^\\]",' /tmp/stackOutput.json
