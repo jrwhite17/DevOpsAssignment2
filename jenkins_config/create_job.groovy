e9cf027a67db1b081db5b033ee60a6a3a6809393
jrwhite17 <johnrileywhite@gmail.com>
2017-08-25 20:53:47 -0500

    test groovy xml job to string

--- jenkins_config/create_job.groovy
+++ jenkins_config/create_job.groovy
@@ -4,8 +4,16 @@ def jobName = "Deploy_ELK_Stack"
 
 //Deploy_ELK_Stack_Jenkins_Job_config.xml is located in /tmp
 //Moved to this location in the userdata portion of the CloudFormation template
-def configXML = new FileInputStream('/tmp/Deploy_ELK_Stack_Jenkins_Job_config.xml')
-def xmlStream = new ByteArrayInputStream( configXML.getBytes() )
+//def configXML = new FileInputStream('/tmp/Deploy_ELK_Stack_Jenkins_Job_config.xml')
+
+//Load XML
+CloudformationData = new XmlSlurper().parse("/tmp/stackData.xml")
+def stackName = CloudformationData.stackName.text()
+
+//String s = new String(bytes, StandardCharsets.UTF_8);
+//def xmlStream = new ByteArrayInputStream( configXML.getBytes() )
+
+String xmlConfig = new File('/tmp/Deploy_ELK_Stack_Jenkins_Job_config.xml').getText('UTF-8')
 
 //Create Job
-Jenkins.instance.createProjectFromXML(jobName, xmlStream)
+Jenkins.instance.createProjectFromXML(jobName, xmlConfig)
